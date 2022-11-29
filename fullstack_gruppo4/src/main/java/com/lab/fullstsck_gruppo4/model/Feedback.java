1e5cbe772552cfcd076d7279dc48ba864f23bac9
fradale2000 <fradale2000@gmail.com>
2022-11-29 15:07:30 +0100

    prova jakacrta

--- fullstack_gruppo4/src/main/java/com/lab/fullstsck_gruppo4/model/Feedback.java
+++ fullstack_gruppo4/src/main/java/com/lab/fullstsck_gruppo4/model/Feedback.java
@@ -1,15 +1,15 @@
 package com.lab.fullstsck_gruppo4.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 
 
@@ -25,7 +25,7 @@ public class Feedback {
 	
 	private String data_feedback;
 		
-	@ManyToOne
+	@jakarta.persistence.ManyToOne
 	@JoinColumn(name = "idutente", nullable = false)
 	private Utenti utenti;
 	
